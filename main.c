cd086b8f61a410c69f1702bcaf566cad0f880532
adamtimps <adamabdelghanyy@icloud.com>
2025-04-30 14:45:11 +0200

    main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "game.h"
+#include "display.h"
+
+int main() {
+    int choice;
+
+    do {
+        display_main_menu();
+        scanf("%d", &choix);
+        switch (choix) {
+            case 1:
+                start_new_game();
+                break;
+            case 2:
+                load_game();  // à coder si tu fais les sauvegardes
+                break;
+            case 3:
+                printf("À bientôt !\n");
+                break;
+            default:
+                printf("Choix invalide.\n");
+        }
+    } while (choix != 3);
+
+    return 0;
+}
