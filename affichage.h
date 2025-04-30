269328b6fa83926d53491722714f4a3e6e7faef5
adamtimps <adamabdelghanyy@icloud.com>
2025-04-30 23:34:32 +0100

    Delete affichage.h

--- affichage.h
+++
@@ -1,7 +0,0 @@
-void display_main_menu() {
-    printf("\n===== CARD YARD =====\n");
-    printf("1. Nouvelle partie\n");
-    printf("2. Charger une partie\n");
-    printf("3. Quitter\n");
-    printf("Choix : ");
-}
