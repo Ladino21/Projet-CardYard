9c6da14af049f7c81644d50e0249488d6f10c85c
Ladino21 <n.mekeddem@hotmail.com>
2025-05-08 13:03:35 +0200

    Update cartes.c

--- cartes.c
+++ cartes.c
@@ -8,8 +8,8 @@
 Pioche creerPiocheDefaut() {
     Pioche pioche;
 
-    int valeursDefaut[] = {-2,-1,0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,};
-    int quantitesDefaut[] = {5, 10, 15, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,};
+    int valeursDefaut[] = {-2,-1,0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int quantitesDefaut[] = {5, 10, 15, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,10};
     int totalValeurs = 14;
 
     int totalCartes = 0;
