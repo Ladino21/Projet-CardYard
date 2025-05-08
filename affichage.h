daca32f2b4cf0ecf9022a788ff36adf4eacfc3d1
adamtimps <adamabdelghanyy@icloud.com>
2025-05-08 19:41:55 +0100

    Create affichage.h

---
+++ affichage.h
@@ -0,0 +1,20 @@
+#ifndef AFFICHAGE_H
+#define AFFICHAGE_H
+
+#include "cartes.h"
+#include "joueurs.h"
+#include "jeu.h"
+
+// Couleurs et mise en forme
+const char* couleurCarte(int valeur);
+
+// Affichages de cartes
+void afficherCarteStylisee(const Carte *carte);
+void afficherLigneCartes(const Carte *cartes, int nbCartes);
+
+// Affichages de zones
+void afficherPiocheCentrale(const Pioche *pioche);
+void afficherDefausse(const Joueur *joueur);
+void afficherPartie(const Partie *partie);
+
+#endif // AFFICHAGE_H
