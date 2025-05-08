347a2d459a16f33d9086a0baa1fa923b8b713789
adamtimps <adamabdelghanyy@icloud.com>
2025-05-08 10:55:37 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -7,14 +7,14 @@
 // Structure représentant l'état global du jeu
 typedef struct {
     int numPlayers;
-    int cardsPerPlayer;
+    int cartesParJoueur;
     int currentPlayer; // index (0-based) du joueur dont c'est le tour
     Deck deck;
     Player *players;
 } Game;
 
 // Crée une nouvelle partie avec les paramètres donnés (renvoie un pointeur vers Game alloué)
-Game* createGame(int numPlayers, int cardsPerPlayer, const char *deckFile);
+Game* createGame(int numPlayers, int cartesParJoueur, const char *deckFile);
 
 // Lance la boucle de jeu interactive pour l'état de jeu donné
 void playGame(Game *game);
