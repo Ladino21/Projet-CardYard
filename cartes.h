a2ac08e5293ddbe287e0deccf9bc69d8aef9ae84
adamtimps <adamabdelghanyy@icloud.com>
2025-05-11 21:29:53 +0100

    Update cartes.h

--- cartes.h
+++ cartes.h
@@ -5,31 +5,29 @@
 
 #define NB_CARTES_MAX 150
 
-// Structure représentant une carte
 typedef struct {
     int valeur;
     bool visible;
 } Carte;
 
-// Structure représentant la pioche (ensemble de cartes à piocher)
 typedef struct {
-    Carte *cartes;  // tableau dynamique de cartes
-    int taille;     // nombre de cartes actuellement dans la pioche
+    Carte *cartes;  
+    int taille;     
 } Pioche;
 
-// Crée une pioche par défaut (jeu de cartes standard)
+
 Pioche creerPiocheDefaut();
 
 // Crée une pioche en chargeant les cartes depuis un fichier "valeur:quantite"
 Pioche creerPiocheDepuisFichier(const char *nomFichier);
 
-// Mélange la pioche
+
 void melangerPioche(Pioche *pioche);
 
-// Pioche une carte du dessus de la pioche
+
 Carte piocherCarte(Pioche *pioche);
 
-// Libère la mémoire allouée pour la pioche
+
 void libererPioche(Pioche *pioche);
 
 #endif // CARTES_H
