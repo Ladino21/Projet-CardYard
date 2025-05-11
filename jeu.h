027f8f5859c032aef766c78434495bce3a1d8fd2
adamtimps <adamabdelghanyy@icloud.com>
2025-05-11 21:31:18 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -4,7 +4,6 @@
 #include "cartes.h"
 #include "joueurs.h"
 
-// Structure représentant l'état global de la partie
 typedef struct {
     int nb_joueurs;
     int nb_cartes_personnelles;
@@ -13,13 +12,13 @@ typedef struct {
     Joueur *joueurs;
 } Partie;
 
-// Crée une nouvelle partie avec les paramètres donnés (renvoie un pointeur alloué)
+
 Partie* creerPartie(int nb_joueurs, int nb_cartes_par_joueur, const char *fichier_pioche);
 
 // Lance la boucle de jeu interactive
 void jouerPartie(Partie *partie);
 
-// Libère toutes les ressources associées à une partie
+
 void libererPartie(Partie *partie);
 
-#endif // JEU_H
+#endif 
