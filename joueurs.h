3d1a9db2a49b5b8be6ef743a3c02200cb6708b90
adamtimps <adamabdelghanyy@icloud.com>
2025-05-11 21:48:24 +0100

    Update joueurs.h

--- joueurs.h
+++ joueurs.h
@@ -5,7 +5,7 @@
 
 #define NB_JOUEURS_MAX 8  // nombre maximal de joueurs
 
-// Structure représentant un joueur
+
 typedef struct {
     Carte *personnelles;     
     int nb_cartes;           
@@ -13,14 +13,8 @@ typedef struct {
     int nb_defausse;         
 } Joueur;
 
-// Crée les joueurs et leur distribue les cartes depuis la pioche
-// nb_joueurs : nombre total de joueurs
-// nb_cartes_par_joueur : nombre de cartes personnelles par joueur
-// pioche : pointeur vers la pioche
-// nb_cartes_total : taille initiale de la pioche (sert à dimensionner les défausses)
 Joueur* creerJoueurs(int nb_joueurs, int nb_cartes_par_joueur, Pioche *pioche, int nb_cartes_total);
 
-// Libère la mémoire allouée pour les joueurs et leurs cartes
 void libererJoueurs(Joueur *joueurs, int nb_joueurs);
 
-#endif // JOUEURS_H
+#endif 
